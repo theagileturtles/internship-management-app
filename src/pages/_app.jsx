6c4608285f04d7065e6f1aaac5a150237501182d
brktsc <buraktasci2001@gmail.com>
2023-04-29 20:22:38 +0300

    header and footer file organization

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {" "}
+      <Head>
+        <title>Internship Management App</title>
+        <meta
+          name="description"
+          content="Internship Management Application which is developed by Agile Turtles"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
