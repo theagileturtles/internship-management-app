cdc8c0bf70722ff59de10339d80f17a7ba3c407c
brktsc <buraktasci2001@gmail.com>
2023-04-29 22:13:27 +0300

    initial commit for the login page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Asap_Condensed, Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Typography } from '@mui/material'
 import Layout from '@/components/layout'
@@ -8,11 +8,12 @@ import Layout from '@/components/layout'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
     <>
 
       <Layout>
-        Heyo
+      Here is the home page
       </Layout>
     </>
   )
