ece70f64c43ea1158d6d9bd25eaed5ae80730e70
brktsc <buraktasci2001@gmail.com>
2023-04-29 22:13:27 +0300

    initial commit for the login page

---
+++ src/pages/auth/login/index.jsx
@@ -0,0 +1,15 @@
+import { Box, Button, Stack, TextInput } from "@mantine/core";
+
+export default function Index() {
+  return (
+    <Box 
+      sx={{height:"100vh" ,display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <Stack spacing={"sm"}>
+        <TextInput placeholder="Email" radius="md" />
+        <TextInput placeholder="Password" radius="md" />
+        <Button radius={"xl"} >Sign In</Button>
+      </Stack>
+    </Box>
+  );
+}
