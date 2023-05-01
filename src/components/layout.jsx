cbae738bd6e433302dab244d30fe6ee6c193b7b1
mehmetali10 <mhmtali2316@gmail.com>
2023-05-01 23:49:43 +0300

    add header component

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,7 +3,7 @@ import Header from "./header";
 export default function Layout({ children }) {
   return (
     <>
-      <Header />
+      <Header userName="Sinan Sensev" userId="200209013 " avatarUrl="https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper-thumbnail.png" />
       <main>{children}</main>
     </>
   );
