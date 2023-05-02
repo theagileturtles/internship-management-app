3981dc04f605757632514f3058e4fe25056ed5be
brktsc <buraktasci2001@gmail.com>
2023-05-02 15:27:22 +0300

    just convert it mantine library to make consistent

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,72 +1,70 @@
-import React from 'react';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import IconButton from '@mui/material/IconButton';
-import LocalPostOfficeRoundedIcon from '@mui/icons-material/LocalPostOfficeRounded';
-import MarkEmailUnreadRoundedIcon from '@mui/icons-material/MarkEmailUnreadRounded';
+import React from "react";
+import { Mail,Bell } from 'tabler-icons-react';
+import { ActionIcon, Avatar, Text } from "@mantine/core";
 
 function Header(props) {
   const headerStyle = {
-    marginTop:"20px",
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '60px',
-    padding: '0 20px',
+    marginTop: "20px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "60px",
+    padding: "0 20px",
   };
 
   const leftStyle = {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   };
 
   const rightStyle = {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
   };
 
   const messageStyle = {
-    marginRight: '20px',
+    marginRight: "20px",
   };
 
   const notificationStyle = {
-    marginRight: '20px',
+    marginRight: "20px",
   };
 
   const userInfoStyle = {
-    marginRight:"14px",
-    display: 'flex',
-    alignItems: 'center',
+    marginRight: "14px",
+    display: "flex",
+    alignItems: "center",
   };
 
   const avatarContainerStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    marginLeft: "20px"
+    display: "flex",
+    alignItems: "center",
+    marginLeft: "20px",
   };
 
   const avatarStyle = {
-    width: '40px',
-    height: '40px',
-    borderRadius: '50%',
-    marginLeft: '10px',
-    objectFit: 'cover',
+    width: "40px",
+    height: "40px",
+    borderRadius: "50%",
+    marginLeft: "10px",
+    objectFit: "cover",
   };
 
   const nameStyle = {
-    fontFamily:"Roboto Flex",
-    fontSize:"20px",
-    marginTop:"10px",
-    fontWeight: 'bold',
-    color: '#333',
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'column',
+    fontFamily: "Roboto Flex",
+    fontSize: "20px",
+    marginTop: "10px",
+    fontWeight: "bold",
+    color: "#333",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
   };
 
   const idStyle = {
-    color: '#999',
-    marginLeft: '10px',
-    fontSize: '15px',
-    fontWeight: 'normal',
+    color: "#999",
+    marginLeft: "10px",
+    fontSize: "15px",
+    fontWeight: "normal",
   };
 
   return (
@@ -74,22 +72,24 @@ function Header(props) {
       <div style={leftStyle}></div>
       <div style={rightStyle}>
         <div style={messageStyle}>
-          <IconButton>
-            <LocalPostOfficeRoundedIcon style={{ color: '#333' }} />
-          </IconButton>
+          <ActionIcon radius={"xl"}>
+            <Mail color="#333333"/>
+          </ActionIcon>
         </div>
         <div style={notificationStyle}>
-          <IconButton>
-            <NotificationsIcon style={{ color: '#333' }} />
-          </IconButton>
+          <ActionIcon radius={"xl"}>
+          <Bell color="#333333"/>
+          </ActionIcon>
         </div>
         <div style={userInfoStyle}>
           <div style={nameStyle}>
-            {props.userName}
-            <div style={idStyle}>{props.userId}</div>
+            <Text>{props.userName}</Text>
+            <div style={idStyle}>
+              <Text>{props.userId}</Text>
+            </div>
           </div>
           <div style={avatarContainerStyle}>
-            <img style={avatarStyle} src={props.avatarUrl} alt="photo" />
+            <Avatar color="dark" sx={avatarStyle} alt={props.userName} />
           </div>
         </div>
       </div>
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -4,7 +4,7 @@ import Footer from "./footer";
 export default function Layout({ children }) {
   return (
     <>
-      <Header userName="Sinan Sensev" userId="200209013 " avatarUrl="https://banner2.cleanpng.com/20180703/ya/kisspng-computer-icons-user-avatar-user-5b3bafe2381423.1933594815306383062297.jpg" />
+      <Header userName="Sinan Sensev" userId="200209013" avatarUrl="https://banner2.cleanpng.com/20180703/ya/kisspng-computer-icons-user-avatar-user-5b3bafe2381423.1933594815306383062297.jpg" />
       <main>{children}</main>
       <Footer />
     </>
