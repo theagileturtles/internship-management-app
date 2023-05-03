4f371bf7ceff3f2c2bd8de8a19f1b09baae3bc80
TraJikar-Mac <mohammad.hameedat@st.uskudar.edu.tr>
2023-05-03 05:21:21 +0300

    better exlanation for each line

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,58 +1,67 @@
+// Importing useState hook from React library for managing state
 import { useState } from 'react';
+// Importing Box, Button, Stack, TextInput, and Checkbox components from Mantine's Core library for styling
 import { Box, Button, Stack, TextInput, Checkbox } from "@mantine/core";
-import logo from "../../../assets/uulogo.png"
+// Importing logo image from assets folder
+import logo from "../../../assets/uulogo.png";
+// Importing Image component from Next.js library for optimized image rendering
 import Image from 'next/image';
 
+
+// LoginPage function component definition
 function LoginPage() {
+
+  // Declare state variables for email, password, and rememberMe using useState
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
-  // Declare three state variables using the useState hook, and initialize them with empty string, empty string and false, respectively.
 
-
+  // Function to handle changes in email input field
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
-  // Declare a function that updates the email state variable with the value of the email input field.
 
+  // Function to handle changes in password input field
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
-  // Declare a function that updates the password state variable with the value of the password input field.
 
+  // Function to handle changes in rememberMe checkbox
   const handleRememberMeChange = (event) => {
     setRememberMe(event.target.checked);
   };
-  // Declare a function that updates the rememberMe state variable with the checked status of the rememberMe checkbox.
 
+  // Function to handle login form submission
   const handleLogin = (event) => {
     event.preventDefault();
     console.log('Login with:', email, password, rememberMe);
   };
-  // Declare a function that prevents the default form submission behavior, and logs the values of email, password and rememberMe to the console.
 
-
+  // Return JSX to render login page
   return (
+    // Outer Box component for positioning and styling the login form
     <Box sx={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: 'white' }}>
-      {/* Render a Box component from the 'mantine' module, with styles for height, display, justifyContent and alignItems. */}
+      {/* Stack component for arranging input fields and buttons vertically */}
       <Stack spacing={"sm"} sx={{ alignItems: 'center' }}>
-        {/* Render a Stack component from the 'mantine' module, with a spacing prop of "sm". */}
+        {/* Image component for displaying logo */}
         <Image src={logo} width={150} height={150} alt="Placeholder image" />
-        {/* Render an Image component from the 'mantine' module, with src, width, height and alt props. */}
+        {/* Box component for displaying the sign in header */}
         <Box sx={{ textAlign: 'center', fontSize: '2rem' }}>
           <h>Sign in to <span style={{ color: '#08B3AD', fontWeight: 'bold' }}>IMA</span></h>
         </Box>
+        {/* TextInput component for email input */}
         <TextInput placeholder="Email" radius="md" value={email} onChange={handleEmailChange} sx={{ width: '300px', borderColor: 'black' }} />
-        {/* Render a TextInput component from the 'mantine' module, with placeholder, radius, value and onChange props. */}
+        {/* TextInput component for password input */}
         <TextInput placeholder="Password" radius="md" value={password} onChange={handlePasswordChange} type="password" sx={{ width: '300px', borderColor: 'black' }} />
-        {/* Render a TextInput component from the 'mantine' module, with placeholder, radius, value, onChange and type props. */}
+        {/* Box component for positioning rememberMe checkbox */}
         <Box sx={{ width: "100%", display: "flex", justifyContent: "left", alignItems: "start" }}>
+          {/* Checkbox component for rememberMe option */}
           <Checkbox label="Remember me" checked={rememberMe} onChange={handleRememberMeChange} />
-          {/* Render a Checkbox component from the 'mantine' module, with label, checked and onChange props. */}
         </Box>
+        {/* Button component for submitting the login form */}
         <Button radius={"xl"} size="sm" sx={{ width: '150px', backgroundColor: '#08B3AD', ':hover': { backgroundColor: '#06A09A' } }} onClick={handleLogin}>Sign In</Button>
-        {/* Render a Button component from the 'mantine' module, with radius and onClick props. */}
       </Stack>
+      {/* Box component for displaying the footer */}
       <Box sx={{ position: 'absolute', bottom: '10px', left: '50%', transform: 'translateX(-50%)', color: 'gray' }}>
         <p>© 2023 Agile Turtles</p>
       </Box>
@@ -60,5 +69,5 @@ function LoginPage() {
   );
 }
 
+// Export the LoginPage function component as the default export
 export default LoginPage;
-// Export the LoginPage component as the default export of this module.
