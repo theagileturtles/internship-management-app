cab98487453c0157b5c0f9c3e24f77f656f762e7
TraJikar-Mac <mohammad.hameedat@st.uskudar.edu.tr>
2023-05-03 05:06:01 +0300

    login last version

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -39,6 +39,9 @@ function LoginPage() {
         {/* Render a Stack component from the 'mantine' module, with a spacing prop of "sm". */}
         <Image src={logo} width={150} height={150} alt="Placeholder image" />
         {/* Render an Image component from the 'mantine' module, with src, width, height and alt props. */}
+        <Box sx={{ textAlign: 'center', fontSize: '2rem' }}>
+          <h>Sign in to <span style={{ color: '#08B3AD', fontWeight: 'bold' }}>IMA</span></h>
+        </Box>
         <TextInput placeholder="Email" radius="md" value={email} onChange={handleEmailChange} sx={{ width: '300px', borderColor: 'black' }} />
         {/* Render a TextInput component from the 'mantine' module, with placeholder, radius, value and onChange props. */}
         <TextInput placeholder="Password" radius="md" value={password} onChange={handlePasswordChange} type="password" sx={{ width: '300px', borderColor: 'black' }} />
@@ -47,9 +50,12 @@ function LoginPage() {
           <Checkbox label="Remember me" checked={rememberMe} onChange={handleRememberMeChange} />
           {/* Render a Checkbox component from the 'mantine' module, with label, checked and onChange props. */}
         </Box>
-        <Button radius={"xl"} size="sm" sx={{ width: '150px' }} onClick={handleLogin}>Sign In</Button>
+        <Button radius={"xl"} size="sm" sx={{ width: '150px', backgroundColor: '#08B3AD', ':hover': { backgroundColor: '#06A09A' } }} onClick={handleLogin}>Sign In</Button>
         {/* Render a Button component from the 'mantine' module, with radius and onClick props. */}
       </Stack>
+      <Box sx={{ position: 'absolute', bottom: '10px', left: '50%', transform: 'translateX(-50%)', color: 'gray' }}>
+        <p>© 2023 Agile Turtles</p>
+      </Box>
     </Box>
   );
 }
