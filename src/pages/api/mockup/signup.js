b200698e8bb02e2040a59732baffaea9ed90f778
brktsc <buraktasci2001@gmail.com>
2023-05-03 03:39:35 +0300

    structure is done

---
+++ src/pages/api/mockup/signup.js
@@ -0,0 +1,45 @@
+import {
+    createConnection,
+    query
+} from "../data_access/database";
+
+export default async function handler(req, res) {
+    const body = req.body
+    if (req.method !== "POST") {
+        res.status(405).json({
+            error: "Method Not Allowed"
+        });
+        return;
+    }
+
+    const connection = createConnection();
+    try {
+        await query(connection)(
+            "INSERT INTO mockup_database.users (first_name, last_name, email, password, role) values(?,?,?,MD5(?),?);",
+            [body.firstName, body.lastName, body.email, body.password, body.role])
+        switch (body.role) {
+            case "student":
+                await query(connection)(
+                    "INSERT INTO mockup_database.students (user_uuid,school_id) values(@last_uuid,?);",
+                    [body.schoolID]
+                )
+                break;
+            case "instructor":
+                await query(connection)(
+                    "INSERT INTO mockup_database.instructors (user_uuid,title) values(@last_uuid,?);",
+                    [body.title]
+                )
+                break;
+            default:
+                break;
+        }
+        res.status(200).json({message:"Done"});
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({error:"Bad Request"});
+    } finally{
+        connection.end()
+    }
+
+
+}
