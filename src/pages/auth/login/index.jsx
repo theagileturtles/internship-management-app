4e455265d4d51a182a0043c908020b6eb10dabe1
brktsc <buraktasci2001@gmail.com>
2023-05-03 13:35:52 +0300

    theme object revised and used linter

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,20 +1,20 @@
 // Importing useState hook from React library for managing state
-import { useState } from 'react';
+import { useState } from "react";
 // Importing Box, Button, Stack, TextInput, and Checkbox components from Mantine's Core library for styling
-import { Box, Button, Stack, TextInput, Checkbox } from "@mantine/core";
+import { Box, Button, Stack, TextInput, Checkbox, useMantineTheme, Title, Text } from "@mantine/core";
 // Importing logo image from assets folder
 import logo from "../../../assets/uulogo.png";
 // Importing Image component from Next.js library for optimized image rendering
-import Image from 'next/image';
-
+import Image from "next/image";
 
 // LoginPage function component definition
 function LoginPage() {
-
   // Declare state variables for email, password, and rememberMe using useState
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
+  const theme = useMantineTheme();
+
 
   // Function to handle changes in email input field
   const handleEmailChange = (event) => {
@@ -34,35 +34,89 @@ function LoginPage() {
   // Function to handle login form submission
   const handleLogin = (event) => {
     event.preventDefault();
-    console.log('Login with:', email, password, rememberMe);
+    console.log("Login with:", email, password, rememberMe);
   };
 
   // Return JSX to render login page
   return (
     // Outer Box component for positioning and styling the login form
-    <Box sx={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: 'white' }}>
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
       {/* Stack component for arranging input fields and buttons vertically */}
-      <Stack spacing={"sm"} sx={{ alignItems: 'center' }}>
+      <Stack spacing={"sm"} sx={{ alignItems: "center" }}>
         {/* Image component for displaying logo */}
         <Image src={logo} width={150} height={150} alt="Placeholder image" />
         {/* Box component for displaying the sign in header */}
-        <Box sx={{ textAlign: 'center', fontSize: '2rem' }}>
-          <h>Sign in to <span style={{ color: '#08B3AD', fontWeight: 'bold' }}>IMA</span></h>
+        <Box sx={{ textAlign: "center", fontSize: "2rem" }}>
+          <Title fw={400}>
+            Sign in to <span style={{color:theme.colors.mainBlue[6],fontWeight: "bold" }}>IMA</span>
+          </Title>
         </Box>
         {/* TextInput component for email input */}
-        <TextInput placeholder="Email" radius="md" value={email} onChange={handleEmailChange} sx={{ width: '300px', borderColor: 'black' }} />
+        <TextInput
+          placeholder="Email"
+          radius="md"
+          value={email}
+          onChange={handleEmailChange}
+          sx={{ width: "300px", borderColor: "black" }}
+        />
         {/* TextInput component for password input */}
-        <TextInput placeholder="Password" radius="md" value={password} onChange={handlePasswordChange} type="password" sx={{ width: '300px', borderColor: 'black' }} />
+        <TextInput
+          placeholder="Password"
+          radius="md"
+          value={password}
+          onChange={handlePasswordChange}
+          type="password"
+          sx={{ width: "300px", borderColor: "black" }}
+        />
         {/* Box component for positioning rememberMe checkbox */}
-        <Box sx={{ width: "100%", display: "flex", justifyContent: "left", alignItems: "start" }}>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "start",
+          }}
+        >
           {/* Checkbox component for rememberMe option */}
-          <Checkbox label="Remember me" checked={rememberMe} onChange={handleRememberMeChange} />
+          <Checkbox
+         
+            label="Remember me"
+            checked={rememberMe}
+            onChange={handleRememberMeChange}
+          />
         </Box>
         {/* Button component for submitting the login form */}
-        <Button radius={"xl"} size="sm" sx={{ width: '150px', backgroundColor: '#08B3AD', ':hover': { backgroundColor: '#06A09A' } }} onClick={handleLogin}>Sign In</Button>
+        <Button
+          radius={"xl"}
+          size="sm"
+          sx={{
+            width: "150px",
+            transition:"0.3s",
+            ":hover": { backgroundColor: theme.colors.mainHoverBlue },
+          }}
+          onClick={handleLogin}
+        >
+          Sign In
+        </Button>
       </Stack>
       {/* Box component for displaying the footer */}
-      <Box sx={{ position: 'absolute', bottom: '10px', left: '50%', transform: 'translateX(-50%)', color: 'gray' }}>
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: "10px",
+          left: "50%",
+          transform: "translateX(-50%)",
+          color: "gray",
+        }}
+      >
         <p>© 2023 Agile Turtles</p>
       </Box>
     </Box>
