a6c237df525cf4eb52c5582a6b7a84b8ac5b1835
brktsc <buraktasci2001@gmail.com>
2023-05-05 03:47:46 +0300

    make more flexible

--- src/pages/student/internship-applications/index.jsx
+++ src/pages/student/internship-applications/index.jsx
@@ -7,10 +7,40 @@ import {
   Title,
   Stack,
   useMantineTheme,
+  Flex,
+  Anchor,
 } from "@mantine/core";
+import Link from "next/link";
+import { Download } from "tabler-icons-react";
+
+function DetailsTitle(props){
+    return(
+        <Text size={"xs"} {...props}/>
+    )
+}
+
+function DetailsText(props){
+    return (
+        <Text size={"sm"} fw={700} {...props}/>
+    )
+}
+
+function TableText(props){
+    return (
+        <Text fw={700} ta={"center"} {...props}/>
+    )
+}
+
+function TableHeader(props){
+    return (
+        <Text ta={"center"} {...props}/>
+    )
+}
 
 export default function Index({ data }) {
   const theme = useMantineTheme();
+
+
 
   return (
     <Layout>
@@ -39,27 +69,22 @@ export default function Index({ data }) {
           <Stack sx={{ width: "100%" }}>
             <Grid pl={"2.875rem"} pr={"1rem"} pt={15}>
               <Grid.Col xs={4}>
-                <Title ta={"center"} order={5}>
-                  Created at
-                </Title>
+                <TableHeader >Created at</TableHeader>
               </Grid.Col>
               <Grid.Col xs={4}>
-                <Title ta={"center"} order={5}>
-                  Internship Type
-                </Title>
+                <TableHeader>Internship Type</TableHeader>
               </Grid.Col>
               <Grid.Col xs={4}>
-                <Title ta={"center"} order={5}>
-                  Status
-                </Title>
+                <TableHeader >Status</TableHeader>
               </Grid.Col>
             </Grid>
             <Accordion color="mainBlue" variant="filled" chevronPosition="left">
-              {data.map((element,index) => (
+              {data.map((element, index) => (
                 <>
-                  <Accordion.Item key={"accordion_item_"+index}
+                  <Accordion.Item
+                    key={"accordion_item_" + index}
                     sx={{
-                        width:"100%",
+                      width: "100%",
                       transition: "0.3s",
                       ":hover": {
                         backgroundColor: "#f9f9f9",
@@ -68,37 +93,67 @@ export default function Index({ data }) {
                         backgroundColor: theme.colors.mainBlue[0],
                       },
                     }}
-                    value={"accordion_item_"+index}
+                    value={"accordion_item_" + index}
                   >
-                    <Accordion.Control>
+                    <Accordion.Control sx={{ width: "100%" }}>
                       <Grid>
                         <Grid.Col xs={4}>
-                          <Text ta={"center"} order={5}>
+                          <TableText >
                             {element.createdAt}
-                          </Text>
+                          </TableText>
                         </Grid.Col>
                         <Grid.Col xs={4}>
-                          <Text ta={"center"} order={5}>
-                          {element.type}
-                          </Text>
+                          <TableText >
+                            {element.type}
+                          </TableText>
                         </Grid.Col>
                         <Grid.Col xs={4}>
-                          <Text ta={"center"} order={5}>
-                          {element.status}
-                          </Text>
+                          <TableText >
+                            {element.status}
+                          </TableText>
                         </Grid.Col>
                       </Grid>
                     </Accordion.Control>
                     <Accordion.Panel>
-                        <Grid sx={{width:"100%", justifyContent:"center", display:"flex"}}>
-                            <Grid.Col xs={6}>
-                                <Text>Files</Text>
-                                
-                            </Grid.Col>
-                            <Grid.Col xs={6}>
-                            <Text>Logs</Text>
-                            </Grid.Col>
-                        </Grid>
+                      <Stack pb={10} pl={"1.7rem"} ta={"center"} spacing={15}>
+                        <Box>
+                          <DetailsTitle>
+                            Department
+                          </DetailsTitle>
+                          <DetailsText>
+                            {element.department}
+                          </DetailsText>
+                        </Box>
+                        <Box>
+                          <DetailsTitle>
+                            Files
+                          </DetailsTitle>
+                          {element.files.map((file) => (
+                            <>
+                              <Flex gap={5} sx={{alignItems:"center", justifyContent:"center"}} direction={"row"}>
+                                <Download size={18}/>
+                                <Anchor target="_blank" href={file.link} download>
+                                  <DetailsText>
+                                    {file.name}
+                                  </DetailsText>
+                                </Anchor>{" "}
+                              </Flex>
+                            </>
+                          ))}
+                        </Box>
+                        <Box>
+                        <DetailsTitle>
+                            Logs
+                          </DetailsTitle>
+                          {element.logs.map((log) => (
+                            <>
+                        <DetailsText >
+                            {log}
+                        </DetailsText>
+                            </>
+                          ))}
+                        </Box>
+                      </Stack>
                     </Accordion.Panel>
                   </Accordion.Item>
                 </>
@@ -121,6 +176,7 @@ export async function getServerSideProps() {
       createdAt: "15.04.2023",
       type: "Voluntary",
       status: "Pending for SGK Entry",
+      department: "Software Engineering",
       files: [
         { name: "Transcript", link: "/" },
         { name: "Application Form", link: "/" },
@@ -134,6 +190,7 @@ export async function getServerSideProps() {
       createdAt: "12.04.2023",
       type: "Compulsory-2",
       status: "Pending for Approvment",
+      department: "Software Engineering",
       files: [
         { name: "Transcript", link: "/" },
         { name: "Application Form", link: "/" },
