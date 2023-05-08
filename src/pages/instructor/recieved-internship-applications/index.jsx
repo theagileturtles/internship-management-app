e8b0c7c56486c3d176a2f933b73984a130340dcb
brktsc <buraktasci2001@gmail.com>
2023-05-08 16:05:22 +0300

    responsive fixes

--- src/pages/instructor/recieved-internship-applications/index.jsx
+++ src/pages/instructor/recieved-internship-applications/index.jsx
@@ -1,7 +1,216 @@
 import Layout from "@/components/layout";
+import {
+  Accordion,
+  Box,
+  Grid,
+  Text,
+  Title,
+  Stack,
+  useMantineTheme,
+  Flex,
+  Anchor,
 
-export default function Index(){
-    return (
-        <Layout role={"instructor"}></Layout>
-    )
+} from "@mantine/core";
+
+import { Download } from "tabler-icons-react";
+
+
+function DetailsTitle(props) {
+  return <Text size={"xs"} {...props} />;
+}
+
+function DetailsText(props) {
+  return <Text size={"sm"} fw={700} {...props} />;
+}
+
+
+function TableText(props) {
+  return <Text fw={700} ta={"center"} {...props} />;
+}
+
+function TableHeader(props) {
+  return <Text ta={"center"} {...props} />;
+}
+
+export default function Index({ data }) {
+
+  const theme = useMantineTheme();
+
+  return (
+    <Layout role={"instructor"}>
+      <Box
+        sx={{
+          height: "100%",
+          width: "100%",
+          paddingLeft: "5vw",
+          paddingRight: "5vw",
+        }}
+      >
+        <Title pb={5} ta={"left"} color="text">
+          RECIEVED INTERNSHIP APPLICATIONS
+        </Title>
+
+
+        <Box
+          sx={{
+            backgroundColor: "white",
+            borderRadius: 50,
+            marginBottom:"50px",
+            minHeight: "50vh",
+            textAlign: "left",
+            justifyContent: "center",
+            display: "flex",
+          }}
+        >
+          <Stack sx={{ width: "100%" }}>
+            <Grid pl={"2.875rem"} pr={"1rem"} pt={15}>
+              <Grid.Col xs={4}>
+                <TableHeader>Created at</TableHeader>
+              </Grid.Col>
+              <Grid.Col xs={4}>
+                <TableHeader>Internship Type</TableHeader>
+              </Grid.Col>
+              <Grid.Col xs={4}>
+                <TableHeader>Status</TableHeader>
+              </Grid.Col>
+            </Grid>
+            <Accordion color="mainBlue" variant="filled" chevronPosition="left">
+              {data.map((element, index) => (
+                <Accordion.Item
+                  key={element.uuid + "_accordion_item_" + index}
+                  sx={{
+                    width: "100%",
+                    transition: "0.3s",
+                    ":hover": {
+                      backgroundColor: "#f9f9f9",
+                    },
+                    "&[data-active]": {
+                      backgroundColor: theme.colors.mainBlue[0],
+                    },
+                  }}
+                  value={"accordion_item_" + index}
+                >
+                  <Accordion.Control sx={{ width: "100%" }}>
+                    <Grid>
+                      <Grid.Col xs={4}>
+                        <TableText>{element.createdAt}</TableText>
+                      </Grid.Col>
+                      <Grid.Col xs={4}>
+                        <TableText>{element.type}</TableText>
+                      </Grid.Col>
+                      <Grid.Col xs={4}>
+                        <TableText>{element.status}</TableText>
+                      </Grid.Col>
+                    </Grid>
+                  </Accordion.Control>
+                  <Accordion.Panel sx={{ color: theme.colors.mainBlue[6] }}>
+                    <Grid grow pl={"2.875rem"} pr={"1rem"} columns={12}>
+                      <Grid.Col
+                        sx={{
+                          justifyContent: "center",
+                          display: "flex",
+                        }}
+                        span={4}
+                      >
+                        <Stack
+                          spacing={0}
+                          sx={{
+                            textAlign: "center",
+                            color: "inherit",
+                          }}
+                        >
+                          <DetailsTitle>Files</DetailsTitle>
+
+                          {element.files.map((file) => (
+                            <Anchor
+                              sx={{ justifyContent: "center", display: "flex" }}
+                              key={element.uuid + "_file_" + file.name}
+                              target="_blank"
+                              href={file.link}
+                              download
+                            >
+                              <Flex gap={3} direction={"row"}>
+                                <Download size={18} />
+                                <DetailsText>{file.name}</DetailsText>
+                              </Flex>
+                            </Anchor>
+                          ))}
+                        </Stack>
+                      </Grid.Col>
+                      <Grid.Col
+                        sx={{
+                          justifyContent: "center",
+                          display: "flex",
+                          minWidth: "fit-content",
+                        }}
+                        span={4}
+                      >
+                        <Stack spacing={0} ta={"center"}>
+                          <DetailsTitle>Logs</DetailsTitle>
+                          {element.logs.map((log, index) => (
+                            <DetailsText
+                              sx={{ color: "inherit" }}
+                              key={element.uuid + "_log_" + index}
+                            >
+                              {log}
+                            </DetailsText>
+                          ))}
+                        </Stack>
+                      </Grid.Col>
+                      <Grid.Col
+                        sx={{
+                          justifyContent: "center",
+                          display: "flex",
+                          minWidth: "fit-content",
+                        }}
+                        span={4}
+                      >
+                        <Box ta={"center"}>
+                          <DetailsTitle>Department</DetailsTitle>
+                          <DetailsText>{element.department}</DetailsText>
+                        </Box>
+                      </Grid.Col>
+                    </Grid>
+                  </Accordion.Panel>
+                </Accordion.Item>
+              ))}
+            </Accordion>
+          </Stack>
+        </Box>
+      </Box>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  const data = [
+    {
+      uuid: "e16f6b76-2e97-48ba-bcf0-52d209bdc0c1",
+      createdAt: "15.04.2023",
+      type: "Voluntary",
+      status: "Pending for SGK Entry",
+      department: "Software Engineering",
+      files: [
+        { name: "Transcript", link: "/" },
+        { name: "Application Form", link: "/" },
+      ],
+      logs: [
+        "created at 15.04.2023 - 23:54",
+        "approved by coordinator 16.04.2023 - 14:02",
+      ],
+    },
+    {
+      uuid: "0855eae4-eb8e-11ed-a05b-0242ac120003",
+      createdAt: "12.04.2023",
+      type: "Compulsory-2",
+      status: "Pending for Approvment",
+      department: "Software Engineering",
+      files: [
+        { name: "Transcript", link: "/" },
+        { name: "Application Form", link: "/" },
+      ],
+      logs: ["created at 12.04.2023 - 23:54"],
+    },
+  ];
+  return { props: { data } };
 }
