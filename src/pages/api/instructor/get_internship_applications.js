5da92044072df81ff006118cb867d56334677220
brktsc <buraktasci2001@gmail.com>
2023-05-14 20:36:57 +0300

    beautify

--- src/pages/api/instructor/get_internship_applications.js
+++ src/pages/api/instructor/get_internship_applications.js
@@ -7,7 +7,7 @@ import sessionExample from "../../../../session-example.json"
 
 export default async function handler(req, res) {
   const session = sessionExample.session;
-  if(!session){
+  if (!session) {
     res.status(401).json({
       error: "Unauthorized"
     });
@@ -25,19 +25,21 @@ export default async function handler(req, res) {
   try {
     connection = createConnection();
     let sql = "SELECT BIN_TO_UUID(users.uuid) AS user_uuid,users.first_name, users.last_name,company,school_id, status, internship_applications.created_at " +
-    "FROM internship_management_app.users, internship_management_app.students, internship_management_app.internship_applications " +
-    "WHERE users.uuid = students.user_uuid AND internship_applications.user_uuid = users.uuid AND students.department_id = ?";
+      "FROM internship_management_app.users, internship_management_app.students, internship_management_app.internship_applications " +
+      "WHERE users.uuid = students.user_uuid AND internship_applications.user_uuid = users.uuid AND students.department_id = ?";
 
-    if(req.query.status){
-      sql+=" AND status = ?"
-    }else{
-      sql+=";"
+    if (req.query.status) {
+      sql += " AND status = ?"
+    } else {
+      sql += ";"
     }
-    
+
 
     const response = await query(connection)(sql,
-    [session.user.departmentID, req.query.status]);
-    res.status(200).json({data: response})
+      [session.user.departmentID, req.query.status]);
+    res.status(200).json({
+      data: response
+    })
 
   } catch (error) {
     res.status(500).json({
