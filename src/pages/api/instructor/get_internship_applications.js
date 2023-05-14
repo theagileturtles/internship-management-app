0fe8f3f6f3fc3c5c8d761e682445474680601e8a
brktsc <buraktasci2001@gmail.com>
2023-05-14 20:36:23 +0300

    get internship applications

--- src/pages/api/instructor/get_internship_applications.js
+++ src/pages/api/instructor/get_internship_applications.js
@@ -1,8 +1,52 @@
 import {
-    createConnection,
-    query
-  } from "../data_access/database";
-  
-  export default async function handler(req, res) {
-    
+  createConnection,
+  query
+} from "../data_access/database";
+
+import sessionExample from "../../../../session-example.json"
+
+export default async function handler(req, res) {
+  const session = sessionExample.session;
+  if(!session){
+    res.status(401).json({
+      error: "Unauthorized"
+    });
+    return
   }
+
+  if (req.method !== "GET") {
+    res.status(405).json({
+      error: "Method Not Allowed"
+    });
+    return;
+  }
+
+  let connection;
+  try {
+    connection = createConnection();
+    let sql = "SELECT BIN_TO_UUID(users.uuid) AS user_uuid,users.first_name, users.last_name,company,school_id, status, internship_applications.created_at " +
+    "FROM internship_management_app.users, internship_management_app.students, internship_management_app.internship_applications " +
+    "WHERE users.uuid = students.user_uuid AND internship_applications.user_uuid = users.uuid AND students.department_id = ?";
+
+    if(req.query.status){
+      sql+=" AND status = ?"
+    }else{
+      sql+=";"
+    }
+    
+
+    const response = await query(connection)(sql,
+    [session.user.departmentID, req.query.status]);
+    res.status(200).json({data: response})
+
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal Server Error"
+    })
+    console.log(error)
+  } finally {
+    if (connection) {
+      connection.end();
+    }
+  }
+}
