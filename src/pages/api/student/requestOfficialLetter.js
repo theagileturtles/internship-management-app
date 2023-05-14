2ee6e8cc4532ab1f1e6fcfd85f0e52fd5f139cc0
mehmetali10 <mhmtali2316@gmail.com>
2023-05-14 23:22:11 +0300

    implement post endpoint for requestOfficalLetter

--- src/pages/api/student/requestOfficialLetter.js
+++ src/pages/api/student/requestOfficialLetter.js
@@ -1,10 +1,64 @@
 import {
-    createConnection,
-    query
-  } from "../data_access/database";
+  createConnection,
+  query
+} from "../data_access/database";
   
+const sessionData = require('./../../../../session-example.json');
+
 //TODO: use auth middleware
-export default function handler(req, res) {
-    //Post -> request offical letter. create tuple for offical_letter_request table
+export default async function handler(req, res) {
+  try {
+    const uuid = sessionData.session.user.uuid;
+
+    if (!sessionData.session) {
+      res.status(401).json({
+        error: "Unauthorized"
+      });
+      return
+    }
+  
+    if (req.method !== "POST") {
+      res.status(405).json({
+        error: "Method Not Allowed"
+      });
+      return;
+    }
+
+    // validate user_uuid
+    if (!isValidBinaryUUID(uuid)) {
+        return res.status(400).json({ message: 'Invalid user_uuid.' });
+    }
+
+    const reqBody = {
+      company : req.body.company,
+      type : req.body.type,
+      message: req.body.message,
+      status: "pending",
+      created_at : new Date()
+    }
+
+    // connect to the database
+    const connection = createConnection();
+    
+    const sql = `
+      INSERT INTO internship_management_app.letter_requests 
+      (user_uuid,company,type,message,status,created_at)
+      values (?, ?, ?, ?, ?, ?)
+    `
+    const q = query(connection);
+    const rows = await q(sql, [uuid, reqBody.company, reqBody.type, reqBody.message, reqBody.status, reqBody.created_at]);
+    // close the database connection
+    connection.end();
+
+    return res.status(200).json({ message : 'letter request saved successfully'});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error.' });
+  }
 }
   
+function isValidBinaryUUID(uuid) {
+  const uuidRegex = /^[a-f\d]{32}$/i;
+  const hex = Buffer.from(uuid, 'binary').toString('hex');
+  return uuidRegex.test(hex);
+}
