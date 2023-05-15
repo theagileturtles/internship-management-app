4253244aab24c212ff9ab18b2b44ca067184dd3d
brktsc <buraktasci2001@gmail.com>
2023-05-15 21:36:31 +0300

    upload form

--- src/pages/api/instructor/get-form-uuid.js
+++ src/pages/api/instructor/get-form-uuid.js
@@ -1,10 +1,10 @@
-
 import {
   createConnection,
   query
 } from "../data_access/database";
 
 import sessionExample from "../../../../session-example.json"
+import AWS from "aws-sdk";
 
 export default async function handler(req, res) {
   const session = sessionExample.session;
@@ -25,28 +25,25 @@ export default async function handler(req, res) {
   let connection;
   try {
     connection = createConnection();
-    let sql = "SELECT BIN_TO_UUID(uuid) AS uuid,created_at FROM internship_management_app.application_forms "+
-    "WHERE department_id = ?";
+    let sql = "SELECT BIN_TO_UUID(uuid) AS uuid,created_at FROM internship_management_app.application_forms " +
+      "WHERE department_id = ?";
 
     const response = await query(connection)(sql,
       [session.user.departmentID, req.query.status]);
 
-      if(response.length === 0){
-        res.status(404).json({
-          data: {
-            url:null
-          }
-        })
-      }else{
-
-        let blob = await fetch(url).then(r => r.blob());
-        
-        res.status(200).json({
-          data: {
-            url: process.env.BASE_S3_URL+"/application-forms/"+response[0].uuid+".pdf"
-          }
-        })
-      }
+    if (response.length === 0) {
+      res.status(404).json({
+        data: {
+          url: null
+        }
+      })
+    } else {
+      res.status(200).json({
+        data: {
+          uuid: response[0].uuid
+        }
+      })
+    }
 
   } catch (error) {
     res.status(500).json({
