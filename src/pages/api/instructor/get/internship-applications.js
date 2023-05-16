66740de5d5f427d2628bb56723dac563e3164893
brktsc <buraktasci2001@gmail.com>
2023-05-16 17:31:04 +0300

    internship appplications endpoint integration

--- src/pages/api/instructor/get/internship-applications.js
+++ src/pages/api/instructor/get/internship-applications.js
@@ -4,6 +4,7 @@ import {
 } from "../../data_access/database";
 
 import sessionExample from "../../../../../session-example.json"
+import { statusConverter, typeConverter } from "@/utils/response-converter";
 
 export default async function handler(req, res) {
   const session = sessionExample.session;
@@ -24,26 +25,42 @@ export default async function handler(req, res) {
   let connection;
   try {
     connection = createConnection();
-    let sql = "SELECT BIN_TO_UUID(internship_applications.uuid) AS UUID,  BIN_TO_UUID(users.uuid) AS userUUID, users.first_name AS firstName, users.last_name AS lastName ,company,school_id AS studentID, status, internship_applications.created_at AS createdAt " +
+    let sql = "SELECT BIN_TO_UUID(internship_applications.uuid) AS UUID,  BIN_TO_UUID(users.uuid) AS userUUID, users.first_name AS firstName, " +
+      "users.last_name AS lastName ,company,school_id AS studentID, status, internship_applications.created_at AS createdAt, internship_applications.type AS type " +
       "FROM internship_management_app.users, internship_management_app.students, internship_management_app.internship_applications " +
       "WHERE users.uuid = students.user_uuid AND internship_applications.user_uuid = users.uuid AND students.department_id = ?";
 
     if (req.query.status) {
-      sql += " AND status = ?"
+      sql += "AND status IN (?)"
     } else {
       sql += ";"
     }
 
-
     let response = await query(connection)(sql,
-      [session.user.departmentID, req.query.status]);
+      [session.user.departmentID, req.query.status?.split(",").map((element)=>element.trim())]);
 
-    response = response.map((element)=>{return {...element, createdAt: new Date(new Date(element.createdAt).getTime() - (new Date(element.createdAt ).getTimezoneOffset() * 60000))}})
+    response = response.map((element) => {
+      return {
+        ...element,
+        createdAt: new Date(new Date(element.createdAt).getTime() - (new Date(element.createdAt).getTimezoneOffset() * 60000)),
+        status:statusConverter(element.status),
+        type: typeConverter(element.type),
+        files: [{
+            name: "Transcript",
+            link: "http://localhost:3000/api/instructor/download/transcript/"+element.userUUID,
+          },
+          {
+            name: "Application Form",
+            link: "http://localhost:3000/api/instructor/download/application-form/"+element.userUUID,
+          }
+        ]
+      }
+    })
 
     res.status(200).json({
       data: response
     })
-    
+
   } catch (error) {
     res.status(500).json({
       error: "Internal Server Error"
