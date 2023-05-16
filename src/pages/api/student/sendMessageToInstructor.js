b40c033061c43f328e4e7f5836393b7899252a75
mehmetali10 <mhmtali2316@gmail.com>
2023-05-16 23:46:46 +0300

    add sendMessageToInstructor endpoint

--- src/pages/api/student/sendMessageToInstructor.js
+++ src/pages/api/student/sendMessageToInstructor.js
@@ -7,16 +7,21 @@ import {
   
   export default async function handler(req, res) {
   
-    if (req.method !== "GET") {
+    if (req.method !== "POST") {
       res.status(405).json({
         error: "Method Not Allowed"
       });
+    }
+
+    if (req.body.subject == "" || req.body.message == "") {
+        res.status(400).json({
+            error: "Subject or Message field cant be empty"
+        })
     }
     
     try {
       const user = sessionData.session.user;
       const uuid = user.uuid;
-      console.log(user)
   
       // check if user_uuid is provided
       if (!uuid) {
@@ -30,18 +35,28 @@ import {
   
       // connect to the database
       const connection = createConnection();
-  
-      // TODO: Select internships that have not passed the application deadline
+
+      const postData = {
+        sender_uuid: uuid,
+        recevier_uuid: "31bd279e-f1d9-11", // this will be updated
+        subject: req.body.subject,
+        message: req.body.message,
+        created_at: new Date()
+      }
+      console.log(postData)
+      
+      // TODO: we should able to reach current student's instructor ids
       const sql = `
-        SELECT header, company, location, department, explanation, start_date, end_date, salary, contact_infos, website
-        FROM internship_management_app.internship_opportinutes
+        INSERT INTO internship_management_app.messages
+        (sender_uuid, recevier_uuid, subject, message, created_at)
+        VALUES(?, ?, ?, ?, ?);
       `;
       const q = query(connection);
-      const rows = await q(sql);
+      const rows = await q(sql, [postData.sender_uuid, postData.recevier_uuid, postData.subject, postData.message, postData.created_at]);
       // close the database connection
       connection.end();
   
-      return res.status(200).json(rows);
+      return res.status(200).json({ message: 'message send successfuly.' });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Internal Server Error.' });
