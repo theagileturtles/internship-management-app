70da4be208280ff75e569d1287fff557e999f982
brktsc <buraktasci2001@gmail.com>
2023-05-16 20:47:01 +0300

    manage internship applications

--- src/pages/instructor/manage-internship-applications/index.jsx
+++ src/pages/instructor/manage-internship-applications/index.jsx
@@ -13,9 +13,13 @@ import {
   Group,
   Pagination,
   Tooltip,
+  Alert,
+  Notification,
+  Transition,
 } from "@mantine/core";
+import { useState } from "react";
 
-import { Download } from "tabler-icons-react";
+import { Check, Download } from "tabler-icons-react";
 
 function DetailsTitle(props) {
   return <Text size={"xs"} {...props} />;
@@ -35,122 +39,206 @@ function TableHeader(props) {
 
 export default function Index({ data }) {
   const theme = useMantineTheme();
+  const [values, setValues] = useState(data);
+  const [notificationVisible, setNotificationVisible] = useState(false);
+  const [notificationData, setNotificationData] = useState({
+    title: "",
+    description: "",
+    icon: <Check />,
+  });
 
+  function approveHandler(event, uuid) {
+    event.stopPropagation();
+    fetch(
+      `/api/instructor/manage/internship-application/${uuid}?action=approve`,
+      {
+        method: "PUT",
+      }
+    ).then((res) => {
+      if (res.status === 200) {
+        fetchData().then((response) => setValues(response));
+        setNotificationData({
+          title: "The Application is Approved!",
+          description: (
+            <Text>
+              You can track the application from{" "}
+              <span style={{ fontWeight: 700 }}>
+                Completed Internship Applications
+              </span>{" "}
+              tab in the sidebar.
+            </Text>
+          ),
+          icon: <Check />,
+        });
+        setNotificationVisible(true);
+      }
+    });
+  }
+
+  function rejectHandler(event, uuid) {
+    event.stopPropagation();
+    fetch(
+      `/api/instructor/manage/internship-application/${uuid}?action=reject`,
+      {
+        method: "PUT",
+      }
+    ).then((res) => {
+      if (res.status === 200) {
+        fetchData().then((response) => setValues(response));
+        setNotificationData({
+          title: "The Application is Rejected!",
+          description: (
+            <Text>
+              The application was sent back to the student. The student will get
+              a notification about that.
+            </Text>
+          ),
+          icon: <Check />,
+        });
+        setNotificationVisible(true);
+      }
+    });
+  }
   return (
-    <Layout role={"instructor"}>
-      <Box
-        sx={{
-          height: "100%",
-          width: "100%",
-          paddingLeft: "5vw",
-          paddingRight: "5vw",
-        }}
-      >
-        <Title pb={5} ta={"left"} color="text">
-        MANAGE INTERNSHIP APPLICATIONS
-        </Title>
-
+    <>
+      <Layout role={"instructor"}>
         <Box
           sx={{
-            backgroundColor: "white",
-            borderRadius: 50,
-            marginBottom: "50px",
-            minHeight: "50vh",
-            textAlign: "left",
-            display: "flex",
-            flexDirection: "column",
+            height: "100%",
+            width: "100%",
+            paddingLeft: "5vw",
+            paddingRight: "5vw",
           }}
         >
-          <Stack pb={20} sx={{ minHeight: "45vh", width: "100%" }}>
-            <Grid pl={"2.875rem"} pr={"1rem"} pt={15}>
-              <Grid.Col xs={6} md={3}>
-                <TableHeader>Created at</TableHeader>
-              </Grid.Col>
-              <Grid.Col xs={6} md={3}>
-                <TableHeader>Student</TableHeader>
-              </Grid.Col>
-              <Grid.Col xs={6} md={3}>
-                <TableHeader>Company</TableHeader>
-              </Grid.Col>
-              <Grid.Col xs={6} md={3}>
-                <TableHeader>Internship Type</TableHeader>
-              </Grid.Col>
-            </Grid>
-            <Accordion color="mainBlue" variant="filled" chevronPosition="left">
-              {data.map((element, index) => (
-                <Accordion.Item
-                  key={element.uuid + "_accordion_item_" + index}
-                  sx={{
-                    width: "100%",
-                    transition: "0.3s",
-                    ":hover": {
-                      backgroundColor: "#f9f9f9",
-                    },
-                    "&[data-active]": {
-                      backgroundColor: theme.colors.mainBlue[0],
-                    },
-                  }}
-                  value={"accordion_item_" + index}
-                >
-                  <Accordion.Control sx={{ width: "100%" }}>
-                    <Grid>
-                    <Tooltip label={new Date(element.createdAt).toLocaleString()}>
-                      <Grid.Col xs={6} md={3}>
-                        <TableText>{new Date(element.createdAt).toLocaleDateString()}</TableText>
-                      </Grid.Col>
-                      </Tooltip>
-                      <Grid.Col xs={6} md={3}>
-                        <TableText>
-                          {element.firstName + " " + element.lastName}
-                        </TableText>
-                      </Grid.Col>
-                      <Grid.Col xs={6} md={3}>
-                        <TableText>{element.company}</TableText>
-                      </Grid.Col>
-                      <Grid.Col xs={6} md={3}>
-                        <TableText>{element.type.label}</TableText>
-                      </Grid.Col>
-                    </Grid>
-                  </Accordion.Control>
-                  <Accordion.Panel
-                    pl={"2.875rem"}
-                    pr={"1rem"}
-                    sx={{ color: theme.colors.mainBlue[6] }}
-                  >
-                    <Grid pb={15} grow columns={12}>
-                      <Grid.Col
-                        sx={{
-                          justifyContent: "center",
-                          display: "flex",
-                        }}
-                        span={4}
-                      >
-                        <Stack
-                          spacing={0}
-                          sx={{
-                            textAlign: "center",
-                            color: "inherit",
-                          }}
-                        >
-                          <DetailsTitle>Files</DetailsTitle>
+          <Title pb={5} ta={"left"} color="text">
+            MANAGE INTERNSHIP APPLICATIONS
+          </Title>
 
-                          {element.files?.map((file) => (
-                            <Anchor
-                              sx={{ justifyContent: "center", display: "flex" }}
-                              key={element.uuid + "_file_" + file.name}
-                              target="_blank"
-                              href={file.link}
-                              download
-                            >
-                              <Flex gap={3} direction={"row"}>
-                                <Download size={18} />
-                                <DetailsText>{file.name}</DetailsText>
-                              </Flex>
-                            </Anchor>
-                          ))}
-                        </Stack>
-                      </Grid.Col>
-                      {/* <Grid.Col
+          <Box
+            sx={{
+              backgroundColor: "white",
+              borderRadius: 50,
+              marginBottom: "50px",
+              minHeight: "50vh",
+              textAlign: "left",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Stack pb={20} sx={{ minHeight: "45vh", width: "100%" }}>
+              <Grid pl={"2.875rem"} pr={"1rem"} pt={15}>
+                <Grid.Col xs={6} md={3}>
+                  <TableHeader>Created at</TableHeader>
+                </Grid.Col>
+                <Grid.Col xs={6} md={3}>
+                  <TableHeader>Student</TableHeader>
+                </Grid.Col>
+                <Grid.Col xs={6} md={3}>
+                  <TableHeader>Company</TableHeader>
+                </Grid.Col>
+                <Grid.Col xs={6} md={3}>
+                  <TableHeader>Internship Type</TableHeader>
+                </Grid.Col>
+              </Grid>
+              {values.length === 0 ? (
+                <Text
+                  pt={"0.5rem"}
+                  fw={700}
+                  fs={"italic"}
+                  color="text"
+                  ta={"center"}
+                >
+                  There is no record now.
+                </Text>
+              ) : (
+                <></>
+              )}
+              <Accordion
+                color="mainBlue"
+                variant="filled"
+                chevronPosition="left"
+              >
+                {values.map((element, index) => (
+                  <Accordion.Item
+                    key={element.uuid + "_accordion_item_" + index}
+                    sx={{
+                      width: "100%",
+                      transition: "0.3s",
+                      ":hover": {
+                        backgroundColor: "#f9f9f9",
+                      },
+                      "&[data-active]": {
+                        backgroundColor: theme.colors.mainBlue[0],
+                      },
+                    }}
+                    value={"accordion_item_" + index}
+                  >
+                    <Accordion.Control sx={{ width: "100%" }}>
+                      <Grid>
+                        <Tooltip
+                          label={new Date(element.createdAt).toLocaleString()}
+                        >
+                          <Grid.Col xs={6} md={3}>
+                            <TableText>
+                              {new Date(element.createdAt).toLocaleDateString()}
+                            </TableText>
+                          </Grid.Col>
+                        </Tooltip>
+                        <Grid.Col xs={6} md={3}>
+                          <TableText>
+                            {element.firstName + " " + element.lastName}
+                          </TableText>
+                        </Grid.Col>
+                        <Grid.Col xs={6} md={3}>
+                          <TableText>{element.company}</TableText>
+                        </Grid.Col>
+                        <Grid.Col xs={6} md={3}>
+                          <TableText>{element.type.label}</TableText>
+                        </Grid.Col>
+                      </Grid>
+                    </Accordion.Control>
+                    <Accordion.Panel
+                      pl={"2.875rem"}
+                      pr={"1rem"}
+                      sx={{ color: theme.colors.mainBlue[6] }}
+                    >
+                      <Grid pb={15} grow columns={12}>
+                        <Grid.Col
+                          sx={{
+                            justifyContent: "center",
+                            display: "flex",
+                          }}
+                          span={4}
+                        >
+                          <Stack
+                            spacing={0}
+                            sx={{
+                              textAlign: "center",
+                              color: "inherit",
+                            }}
+                          >
+                            <DetailsTitle>Files</DetailsTitle>
+
+                            {element.files?.map((file) => (
+                              <Anchor
+                                sx={{
+                                  justifyContent: "center",
+                                  display: "flex",
+                                }}
+                                key={element.uuid + "_file_" + file.name}
+                                target="_blank"
+                                href={file.link}
+                                download
+                              >
+                                <Flex gap={3} direction={"row"}>
+                                  <Download size={18} />
+                                  <DetailsText>{file.name}</DetailsText>
+                                </Flex>
+                              </Anchor>
+                            ))}
+                          </Stack>
+                        </Grid.Col>
+                        {/* <Grid.Col
                         sx={{
                           justifyContent: "center",
                           display: "flex",
@@ -170,58 +258,87 @@ export default function Index({ data }) {
                           ))}
                         </Stack>
                       </Grid.Col> */}
-                      <Grid.Col
-                        sx={{
-                          justifyContent: "center",
-                          display: "flex",
-                          minWidth: "fit-content",
-                        }}
-                        span={4}
-                      >
-                        <Box ta={"center"}>
-                          <DetailsTitle>Student No</DetailsTitle>
-                          <DetailsText>{element.studentID}</DetailsText>
-                        </Box>
-                      </Grid.Col>
-                    </Grid>
-                    <Flex sx={{ justifyContent: "center" }}>
-                      <Group>
-                        <Button
-                          radius={"xl"}
-                          onClick={(event) => {
-                            event.stopPropagation();
+                        <Grid.Col
+                          sx={{
+                            justifyContent: "center",
+                            display: "flex",
+                            minWidth: "fit-content",
                           }}
-                          sx={{ width: "100px" }}
+                          span={4}
                         >
-                          Approve
-                        </Button>
-                        <Button
-                          color="red"
-                          radius={"xl"}
-                          onClick={(event) => {
-                            event.stopPropagation();
-                          }}
-                          sx={{ width: "100px" }}
-                        >
-                          Reject
-                        </Button>
-                      </Group>
-                    </Flex>
-                  </Accordion.Panel>
-                </Accordion.Item>
-              ))}
-            </Accordion>
-          </Stack>
-          <Pagination mb={"sm"} sx={{ alignSelf: "center" }} total={1} />
+                          <Box ta={"center"}>
+                            <DetailsTitle>Student No</DetailsTitle>
+                            <DetailsText>{element.studentID}</DetailsText>
+                          </Box>
+                        </Grid.Col>
+                      </Grid>
+                      <Flex sx={{ justifyContent: "center" }}>
+                        <Group>
+                          <Button
+                            radius={"xl"}
+                            onClick={(event) =>
+                              approveHandler(event, element.UUID)
+                            }
+                            sx={{ width: "100px" }}
+                          >
+                            Approve
+                          </Button>
+                          <Button
+                            color="red"
+                            radius={"xl"}
+                            onClick={(event) =>
+                              rejectHandler(event, element.UUID)
+                            }
+                            sx={{ width: "100px" }}
+                          >
+                            Reject
+                          </Button>
+                        </Group>
+                      </Flex>
+                    </Accordion.Panel>
+                  </Accordion.Item>
+                ))}
+              </Accordion>
+            </Stack>
+            <Pagination mb={"sm"} sx={{ alignSelf: "center" }} total={1} />
+          </Box>
         </Box>
-      </Box>
-    </Layout>
+      </Layout>
+      <Transition
+        mounted={notificationVisible}
+        transition="fade"
+        duration={200}
+        timingFunction="ease"
+      >
+        {(styles) => (
+          <Notification
+            onClose={() => {
+              setNotificationVisible(false);
+            }}
+            withCloseButton
+            style={styles}
+            sx={{ position: "fixed", bottom: "3rem", left: "3rem" }}
+            icon={<Check size="1.1rem" />}
+            color="mainBlue"
+            title={notificationData.title}
+          >
+            {notificationData.description}
+          </Notification>
+        )}
+      </Transition>
+    </>
   );
 }
 
 export async function getServerSideProps() {
-  const response = await fetch("http://localhost:3000/api/instructor/get/internship-applications?status=pending_for_coordinator").then((res)=>res.json())
-  
-  const data = [...response.data];
+  const data = await fetchData();
   return { props: { data } };
+}
+
+async function fetchData() {
+  const response = await fetch(
+    "http://localhost:3000/api/instructor/get/internship-applications?status=pending_for_coordinator"
+  ).then((res) => res.json());
+
+  return [...response.data];
 }
