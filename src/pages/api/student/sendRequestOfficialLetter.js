1c6488a6fcd9f1b2b98632baa5279b3333c90db9
brktsc <buraktasci2001@gmail.com>
2023-05-21 04:00:45 +0300

    official letter request

--- src/pages/api/student/sendRequestOfficialLetter.js
+++ src/pages/api/student/sendRequestOfficialLetter.js
@@ -2,12 +2,21 @@ import {
   createConnection,
   query
 } from "../data_access/database";
-  
+
 const sessionData = require('./../../../../session-example.json');
+import AWS from "aws-sdk";
+
+export const config = {
+  api: {
+      bodyParser: {
+          sizeLimit: '4mb'
+      }
+  }
+}
 
 //TODO: use auth middleware
 export default async function handler(req, res) {
-  
+
   if (req.method !== "POST") {
     res.status(405).json({
       error: "Method Not Allowed"
@@ -16,7 +25,6 @@ export default async function handler(req, res) {
   }
 
   try {
-    const uuid = sessionData.session.user.uuid;
 
     if (!sessionData.session) {
       res.status(401).json({
@@ -24,40 +32,64 @@ export default async function handler(req, res) {
       });
       return
     }
-  
+
     // validate user_uuid
     // if (!isValidBinaryUUID(uuid)) {
     //     return res.status(400).json({ message: 'Invalid user_uuid.' });
     // }
 
-    const reqBody = {
-      company : req.body.company,
-      type : req.body.type,
-      message: req.body.message,
-    }
+    const reqBody = JSON.parse(req.body);
 
     // connect to the database
     const connection = createConnection();
-    
-    const sql = `
-      INSERT INTO internship_management_app.letter_requests 
-      (user_uuid,company,type,message)
-      values (UUID_TO_BIN(?), ?, ?, ?)
-    `
-    const q = query(connection);
-    const rows = await q(sql, [uuid, reqBody.company, reqBody.type, reqBody.message]);
-    // close the database connection
-    connection.end();
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+      region: process.env.AWS_S3_REGION
+    });
 
-    return res.status(200).json({ message : 'letter request saved successfully'});
+    let uuid = await query(connection)(
+      "SELECT UUID() as uuid")
+    uuid = uuid[0].uuid
+    await uploadToS3(s3, reqBody.blob, uuid, "transcript").then(async () => {
+      const sql = `
+    INSERT INTO internship_management_app.letter_requests 
+    (uuid,user_uuid,company,type,message, incomplete_internship)
+    values (UUID_TO_BIN(?),UUID_TO_BIN(?), ?, ?, ?, ?)
+  `
+      const q = query(connection);
+      const rows = await q(sql, [uuid,sessionData.session.user.uuid, reqBody.company, reqBody.type, reqBody.message,reqBody.numberOfIncomplete]);
+      // close the database connection
+      connection.end();
+
+      return res.status(200).json({
+        message: 'letter request saved successfully'
+      });
+    }).catch(()=>{
+      return res.status(500).json({
+        message: 'Internal Server Error.'
+      });
+    });
+
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Internal Server Error.' });
+    return res.status(500).json({
+      message: 'Internal Server Error.'
+    });
   }
 }
-  
-function isValidBinaryUUID(uuid) {
-  const uuidRegex = /^[a-f\d]{32}$/i;
-  const hex = Buffer.from(uuid, 'binary').toString('hex');
-  return uuidRegex.test(hex);
+
+
+async function uploadToS3(s3, blob, uuid, filename) {
+  const base64 = blob;
+  const base64Data = Buffer.from(base64.replace(/^data:application\/\w+;base64,/, ""), "base64");
+  console.log("ekledi"+filename)
+  return s3
+    .upload({
+      Bucket: process.env.AWS_S3_BUCKET_NAME + "/letter-requests/" + uuid,
+      Key: filename + ".pdf",
+      Body: base64Data,
+      ContentEncoding: "base64",
+      ContentType: "application/pdf"
+    }).promise()
 }
--- src/pages/student/request-for-an-official-letter/index.jsx
+++ src/pages/student/request-for-an-official-letter/index.jsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/layout";
+import UploadInput from "@/components/uploadinput";
 import {
   Box,
   Title,
@@ -13,113 +14,310 @@ import {
   FileInput,
   Center,
   Textarea,
+  ActionIcon,
+  Group,
+  Select,
+  Transition,
+  Notification,
 } from "@mantine/core";
 import { useState } from "react";
-import { Checkbox, Download, Tex, Upload } from "tabler-icons-react";
-   
+import {
+  Check,
+  Checkbox,
+  Download,
+  Tex,
+  Trash,
+  Upload,
+  X,
+} from "tabler-icons-react";
+
 const MAX_LETTER = 150;
 export default function Index() {
   const theme = useMantineTheme();
 
-  const [companyName, setCompanyName] = useState();
-  const [type, setType] = useState();
-  const [transcript, setTranscript] = useState();
-  const [message, setMessage] = useState();
+  const [company, setCompany] = useState("");
+  const [type, setType] = useState(null);
+  const [transcript, setTranscript] = useState(null);
+  const [message, setMessage] = useState("");
+  const [numberOfIncomplete, setNumberOfIncomplete] = useState(null);
+  const [transcriptHref, setTranscriptHref] = useState(null);
+  const [notificationVisible, setNotificationVisible] = useState(false);
+  const [notificationData, setNotificationData] = useState({
+    title: "",
+    description: "",
+    icon: <Check />,
+  });
+  const [requestLoader, setRequestLoader] = useState(false);
 
-  const defaultValues = {
-    company:"",
-    type:"",
-    transcript:"",
-    message:"",
+  function messageHandler(event) {
+    setMessage(event.target.value);
   }
 
-  const [values, setValues] = useState(defaultValues)
+  function companyHandler(event) {
+    setCompany(event.target.value);
+  }
 
-  function messageHandler(event){
-    setValues({...values,message:event.target.value})
+  function transcriptHandler(file) {
+    if (file) {
+      setTranscript(file);
+      toBase64(file).then((blob) => setTranscriptHref(blob));
+    }
+  }
+
+  function numberOfIncompleteHandler(event) {
+    setNumberOfIncomplete(event);
+  }
+
+  function typeHandler(event) {
+    setType(event);
+  }
+
+  function requestHandler() {
+    setRequestLoader(true);
+    fetch("/api/student/sendRequestOfficialLetter", {
+      method: "POST",
+      body: JSON.stringify({
+        blob: transcriptHref,
+        company: company,
+        type: type,
+        message: message,
+        numberOfIncomplete: numberOfIncomplete,
+      }),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setNotificationData({
+            title: "The Official Letter is Requested Successfully!",
+            description: (
+              <Text>
+                You can track the request via{" "}
+                <span style={{ fontWeight: 700 }}>Official Letter Request</span>{" "}
+                tab in the sidebar.
+              </Text>
+            ),
+            icon: <Check />,
+          });
+        } else {
+          setNotificationData({
+            title: "Error Occured",
+            description: (
+              <Text>
+                Error occured while you are creating your application. Please
+                try again later.
+              </Text>
+            ),
+            color: "red",
+            icon: <X />,
+          });
+        }
+        setNotificationVisible(true);
+      })
+      .finally(() => {
+        setRequestLoader(false);
+        setCompany("");
+        setType(null);
+        setTranscript(null);
+        setMessage("");
+        setNumberOfIncomplete(null);
+      });
   }
 
   return (
-    <Layout role={"student"}>
-      <Box
-        sx={{
-          minHeight: "100%",
-          width: "100%",
-          paddingLeft: "5vw",
-          paddingRight: "5vw",
-        }}
-      >
-        {/* Title for the page. pb means padding bottom which creates space between bottom part. ta means text align. Also you can define
-        these features in sx prop which helps to create custom css on the specific mantine component*/}
-        <Title pb={5} ta={"left"} color="text">
-          REQUEST FOR AN OFFICIAL LETTER
-        </Title>
+    <>
+      <Layout role={"student"}>
         <Box
-          pl={"3rem"}
-          pr={"3rem"}
-          pt={"1.5rem"}
-          pb={"1.5rem"}
           sx={{
-            backgroundColor: "white",
-            borderRadius: 50,
+            minHeight: "100%",
             width: "100%",
-            minHeight: "50vh",
-            textAlign: "left",
-            display: "flex",
+            paddingLeft: "5vw",
+            paddingRight: "5vw",
           }}
         >
-          <Grid gutter={"xl"}>
-            <Grid.Col span={12}>
-              <TextInput
-                sx={{ maxWidth: 250 }}
-                radius={"xl"}
-                placeholder="Company Name You Applied"
-                label="Company Name"
-                withAsterisk
-              />
-            </Grid.Col>
-
-            <Grid.Col xs={12} sm={12} lg={6} xl={6}>
-              <Radio.Group
-                name="typeOfInternship"
-                label="Choose Internship Type"
-                withAsterisk
-              >
-                <Stack mt="xs">
-                  <Radio value="compulsary1" label="Compulsary-1" />
-                  <Radio value="compulsary2" label="Compulsary-2" />
-                  <Radio value="voluntary" label="Voluntary" />
-                </Stack>
-              </Radio.Group>
-            </Grid.Col>
-            <Grid.Col xs={12} sm={12} lg={6} xl={6}>
-              <Stack>
-
-            <FileInput
-                      icon={<Upload size={"1.1rem"} />}
-                      variant="filled"
-                      placeholder="Your Transcript"
-                      label="Transcript"
-                      radius="xl"
+          {/* Title for the page. pb means padding bottom which creates space between bottom part. ta means text align. Also you can define
+        these features in sx prop which helps to create custom css on the specific mantine component*/}
+          <Title pb={5} ta={"left"} color="text">
+            REQUEST FOR AN OFFICIAL LETTER
+          </Title>
+          <Box
+            pl={"3rem"}
+            pr={"3rem"}
+            pt={"1.5rem"}
+            pb={"1.5rem"}
+            sx={{
+              backgroundColor: "white",
+              borderRadius: 50,
+              width: "100%",
+              minHeight: "50vh",
+              textAlign: "left",
+              display: "flex",
+            }}
+          >
+            <Grid align="start" gutter={"xl"}>
+              <Grid.Col sm={12} lg={6}>
+                <TextInput
+                  sx={{ maxWidth: 250 }}
+                  onChange={companyHandler}
+                  value={company}
+                  radius={"xl"}
+                  placeholder="Company Name You Applied"
+                  label="Company Name"
+                  withAsterisk
+                />
+              </Grid.Col>
+              <Grid.Col sm={12} lg={6}>
+                <Select
+                  onChange={numberOfIncompleteHandler}
+                  value={numberOfIncomplete}
+                  sx={{ maxWidth: "250px" }}
+                  label="Number of incomplete internships"
+                  placeholder="Pick one"
+                  withAsterisk
+                  radius={"xl"}
+                  data={[
+                    { value: "0", label: "0" },
+                    { value: "1", label: "1" },
+                    { value: "2", label: "2" },
+                  ]}
+                />
+              </Grid.Col>
+              <Grid.Col xs={12} sm={12} lg={6} xl={6}>
+                <Radio.Group
+                  onChange={typeHandler}
+                  value={type}
+                  name="typeOfInternship"
+                  label="Choose Internship Type"
+                  withAsterisk
+                >
+                  <Stack mt="xs">
+                    <Radio value="compulsory1" label="Compulsary-1" />
+                    <Radio value="compulsory2" label="Compulsary-2" />
+                    <Radio value="voluntary" label="Voluntary" />
+                  </Stack>
+                </Radio.Group>
+              </Grid.Col>
+              <Grid.Col xs={12} sm={12} lg={6} xl={6}>
+                <Stack spacing={33}>
+                  <Group
+                    spacing={2}
+                    sx={{
+                      width: "100%",
+                      alignItems: "end",
+                    }}
+                  >
+                    <UploadInput
+                      value={transcript}
                       withAsterisk
-                      sx={{ maxWidth: 300 }}
+                      onChange={transcriptHandler}
+                      variant="filled"
+                      icon={<Upload size={"1.1rem"} />}
+                      placeholder="Your Transcript"
+                      label={"Transcript"}
+                      radius={"xl"}
+                      sx={{ width: "100%", maxWidth: "300px" }}
                     />
-                    <Stack display={"flex"} spacing={2}>
-                    <Textarea onChange={messageHandler} autosize minRows={3} maxRows={3} radius={"md"} maxLength={150} placeholder="Additional Information" label="Message"/>
-                      <Text  size={"sm"} sx={{alignSelf:"end"}} color="dimmed">{values.message.length}/{MAX_LETTER}</Text>
-                    </Stack>
-              </Stack>
-            </Grid.Col>
-            <Grid.Col xl={12}>
-              <Center>
-                <Button sx={{ width: "100%", maxWidth: 200 }} radius={"xl"}>
-                  Request
-                </Button>
-              </Center>
-            </Grid.Col>
-          </Grid>
+                    {transcript ? (
+                      <Group spacing={2}>
+                        <ActionIcon
+                          color="mainBlue"
+                          component="a"
+                          download={transcript.name}
+                          href={transcriptHref}
+                          target="_blank"
+                          mb={"0.3rem"}
+                          radius={"xl"}
+                        >
+                          <Download size={"1.3rem"} />
+                        </ActionIcon>
+
+                        <ActionIcon
+                          onClick={() => {
+                            setTranscript(null);
+                          }}
+                          color="red"
+                          mb={"0.3rem"}
+                          radius={"xl"}
+                        >
+                          <Trash size={"1.3rem"} />
+                        </ActionIcon>
+                      </Group>
+                    ) : (
+                      <></>
+                    )}
+                  </Group>
+                  <Stack display={"flex"} spacing={2}>
+                    <Textarea
+                      onChange={messageHandler}
+                      autosize
+                      value={message}
+                      minRows={3}
+                      maxRows={3}
+                      radius={"md"}
+                      maxLength={150}
+                      placeholder="Additional Information"
+                      label="Message"
+                    />
+                    <Text size={"sm"} sx={{ alignSelf: "end" }} color="dimmed">
+                      {message.length}/{MAX_LETTER}
+                    </Text>
+                  </Stack>
+                </Stack>
+              </Grid.Col>
+              <Grid.Col xl={12}>
+                <Center>
+                  <Button
+                  loading={requestLoader}
+                    onClick={requestHandler}
+                    disabled={
+                      !(
+                        company.length > 0 &&
+                        type &&
+                        transcript &&
+                        numberOfIncomplete
+                      )
+                    }
+                    sx={{ width: "100%", maxWidth: 200 }}
+                    radius={"xl"}
+                  >
+                    Request
+                  </Button>
+                </Center>
+              </Grid.Col>
+            </Grid>
+          </Box>
         </Box>
-      </Box>
-    </Layout>
+      </Layout>
+      <Transition
+        mounted={notificationVisible}
+        transition="fade"
+        duration={200}
+        timingFunction="ease"
+      >
+        {(styles) => (
+          <Notification
+            onClose={() => {
+              setNotificationVisible(false);
+            }}
+            withCloseButton
+            style={styles}
+            sx={{ position: "fixed", bottom: "3rem", left: "3rem" }}
+            icon={notificationData.icon}
+            color={notificationData.color}
+            title={notificationData.title}
+          >
+            {notificationData.description}
+          </Notification>
+        )}
+      </Transition>
+    </>
   );
+}
+
+function toBase64(blob) {
+  const reader = new FileReader();
+  return new Promise((res, rej) => {
+    reader.readAsDataURL(blob);
+    reader.onload = function () {
+      res(reader.result);
+    };
+  });
 }
