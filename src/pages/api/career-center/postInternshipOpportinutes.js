49b19b6c410366560c67c750d98b869d306624ec
mehmetali10 <mhmtali2316@gmail.com>
2023-05-22 23:17:46 +0300

    add endpoint for intership-opportinutes

--- src/pages/api/career-center/postInternshipOpportinutes.js
+++ src/pages/api/career-center/postInternshipOpportinutes.js
@@ -5,10 +5,7 @@ import {
 
 const sessionData = require('./../../../../session-example.json');
 
-
-
-
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
   if (req.method !== "POST") {
     res.status(405).json({
@@ -22,14 +19,48 @@ export default function handler(req, res) {
       })
   }
 
+  const user = sessionData.session.user;
+  const uuid = user.uuid;
 
-    if (req.method === "POST") {
-      const { companyName, type, fileName, description, applicationPage } = req.body;
-      console.log(companyName, " ", type, " ", fileName, " ", description, " ",applicationPage)
-  
-      res.status(200).json({ message: "Publish successful" });
-    } else {
-      res.status(405).json({ message: "Method Not Allowed" });
-    }
+  // check if user_uuid is provided
+  if (!uuid) {
+    return res.status(400).json({ message: 'invalid session id' });
   }
+
+  try {
+      
+    const connection = createConnection();
+
+    const postData = {
+      companyName: req.body.companyName,
+      type: req.body.type,
+      fileName: req.body.fileName,
+      description: req.body.description,
+      applicationPage: req.body.applicationPage,
+      created_at: new Date()
+    }
+    console.log(postData)
+
+    const sql = `
+    INSERT INTO internship_management_app.internship_opportunities
+    (header, company, explanation, website, type, created_at)
+    VALUES(?, ?, ?, ?, ?, ?);
+  `;
+    const q = query(connection);
+    const rows = await q(sql, [postData.companyName, 
+                            postData.companyName, 
+                            postData.description,
+                            postData.applicationPage, 
+                            postData.type, 
+                            postData.created_at]);
+
+    // close the database connection
+    connection.end();
+    res.status(200).json({ message: "Publish successful" });
+
+  } catch(error) {
+      return res.status(500).json({ message: 'Internal Server Error.' });
+  }
+
+}
   
