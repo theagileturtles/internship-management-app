edbfde8c0212d11866dd8db785d3ad8d2300b1dc
brktsc <buraktasci2001@gmail.com>
2023-05-31 02:02:52 +0300

    admin career center

---
+++ src/pages/api/admin/manage_career-center-staff.js
@@ -0,0 +1,42 @@
+import {
+    createConnection,
+    query
+  } from "../data_access/database";
+  
+import sessionExample from "../../../../session-example.json"
+
+export default async function handler(req,res){
+    const session = sessionExample.session;
+
+    if(!session){
+        res.status(401)
+    }
+
+    if(req.method!=="PUT"){
+        res.status(405).json({error:"Method not allowed"})
+    }
+    const body = JSON.parse(req.body)
+    let promises = []
+    let connection;
+    try {
+        connection =  createConnection();
+        body.forEach(async (element)=>{
+            promises.push(await query(connection)("UPDATE internship_management_app.users SET role_id = ? WHERE user_uuid = UUID_TO_BIN(?)",
+            [element.roleID, element.UUID]))
+        })
+        Promise.all(promises).then(()=>{
+            res.status(200).json({message:"Updated Successfully"})
+        }).catch((err)=>{
+            console.log(err)
+            res.status(400).json({error:"Internal Server Error"})
+
+        })
+    } catch (error) {
+        res.status(400).json({error:"Internal Server Error"})
+        console.log(error)
+    }finally{
+        if(connection){
+            connection.end()
+        }
+    }
+}
